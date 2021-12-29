6829709bc1ae89127eef2b5b99ae380de58bc34d
tazzanmc <taz@25.4.230.53>
2021-12-29 14:37:23 -0500

    Expanded embers, Pyretic composite

---
+++ src/main/java/net/mcreator/floral_fantasy/item/PyreticCompositeItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.floral_fantasy.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.floral_fantasy.FloralFantasyModElements;
+
+@FloralFantasyModElements.ModElement.Tag
+public class PyreticCompositeItem extends FloralFantasyModElements.ModElement {
+	@ObjectHolder("floral_fantasy:pyretic_composite")
+	public static final Item block = null;
+	public PyreticCompositeItem(FloralFantasyModElements instance) {
+		super(instance, 230);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("pyretic_composite");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
--- src/main/java/net/mcreator/floral_fantasy/procedures/IgnitionOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/floral_fantasy/procedures/IgnitionOnEffectActiveTickProcedure.java
@@ -44,8 +44,7 @@ public class IgnitionOnEffectActiveTickProcedure {
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if (((!(entity.isSneaking()))
-				|| ((((entity.getMotion().getX()) >= 0.5) || ((entity.getMotion().getZ()) >= 0.5)) || ((entity.getMotion().getY()) >= 0.5)))) {
+		if ((!(entity.isSneaking()))) {
 			entity.setFire((int) (1 + ((new Object() {
 				int check(Entity _entity) {
 					if (_entity instanceof LivingEntity) {
